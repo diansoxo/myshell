ba7a3bffdeb9df4b3a58d88ff9d74d380c6715c6
diansoxo <mursalimova.diana@bk.ru>
2025-09-24 20:43:48 +0500

    Add files via upload

---
+++ inc/lexer.h
@@ -0,0 +1,25 @@
+#ifndef LEXER_H
+#define LEXER_H
+
+#include "ast.h"
+
+typedef struct {
+    const char *input;
+    int position;
+    int length;
+    token_t *tokens;
+    token_t *current_token;
+} lexer_t;
+
+lexer_t *lexer_create(const char *input);
+void lexer_destroy(lexer_t *lexer);
+token_t *lexer_tokenize(lexer_t *lexer);
+token_t *lexer_get_next_token(lexer_t *lexer);
+void lexer_rewind(lexer_t *lexer);
+
+int is_special_char(char c);
+int is_whitespace(char c);
+char *handle_quotes(lexer_t *lexer, char quote_type);
+char *handle_word(lexer_t *lexer);
+
+#endif
