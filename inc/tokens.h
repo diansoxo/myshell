a7e750b6272ae399a7777807fec1b398a9ce669e
diansoxo <mursalimova.diana@bk.ru>
2025-10-04 19:47:55 +0500

    Update tokens.h

--- inc/tokens.h
+++ inc/tokens.h
@@ -1,5 +1,5 @@
-#ifndef AST_H
-#define AST_H
+#ifndef TOKENS_H
+#define TOKENS_H
 
 typedef enum {
     TOKEN_WORD,
@@ -22,44 +22,5 @@ typedef struct token {
     char *value;
     struct token *next;
 } token_t;
-
-typedef enum {
-    NODE_SIMPLE_COMMAND,
-    NODE_PIPE,
-    NODE_REDIRECT,
-    NODE_AND,
-    NODE_OR,
-    NODE_SEMICOLON,
-    NODE_BACKGROUND
-} node_type_t;
-
-typedef struct ast_node {
-    node_type_t type;
-    
-    union {
-        struct {
-            char **args;
-            int argc;
-        } command;
-        
-        struct {
-            struct ast_node *left;
-            struct ast_node *right;
-        } binary;
-        
-
-        struct {
-            struct ast_node *child;
-            char *filename;
-            int flags;
-            int fd;
-        } redirect;
-    } data;
-} ast_node_t;
-
-ast_node_t *create_command_node(char **args, int argc);
-ast_node_t *create_binary_node(node_type_t type, ast_node_t *left, ast_node_t *right);
-ast_node_t *create_redirect_node(ast_node_t *child, char *filename, int flags, int fd);
-void free_ast(ast_node_t *node);
 
 #endif
