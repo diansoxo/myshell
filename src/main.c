14f0cf23ed6b222ee7df74f6387a47bff03c2bb5
diansoxo <mursalimova.diana@bk.ru>
2025-10-12 17:48:28 +0500

    Add files via upload

---
+++ src/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lexer.h"
+
+int main() {
+    printf("Testing lexer...\n\n");
+    
+    char *test = "ls -l | grep hello";
+    
+    lexer_t *lexer = lexer_create(test);
+
+    token_t *tokens = lexer_tokenize(lexer);
+    
+    token_t *current = tokens;
+    while (current) {
+        printf("Token: %d, Value: %s\n", 
+               current->type, 
+               current->value ? current->value : "NULL");
+        current = current->next;
+    }
+    
+    lexer_destroy(lexer);
+    
+    printf("\ntest completed!\n");
+    return 0;
+}
