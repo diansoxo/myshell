089458b9cb88cd870425b466a52c023f39bb4d5d
Diana Mursalimova <mursalimova.diana@bk.ru>
2025-11-19 23:54:55 +0500

    Update shell.h
    Removed unnecessary includes and added function prototype.

--- inc/shell.h
+++ inc/shell.h
@@ -1,17 +1,16 @@
 #ifndef SHELL_H
 #define SHELL_H
 
-#include "lexer.h"
-#include "parser.h"
-#include "ast.h"
-
 typedef struct {
     int running;
     char *prompt;
 } shell_t;
 
-shell_t *shell_create();
+shell_t *shell_create(void);
 void shell_destroy(shell_t *shell);
 void shell_run(shell_t *shell);
 
 #endif
+
+//работает ли она сейчас и объявляет три главные функции
+// создать оболочку, запустить ее главный цикл и уничтожить при выходе
