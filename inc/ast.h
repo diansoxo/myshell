dabffdcba5f870750eb9aa727367bfe203bd7b15
Diana Mursalimova <mursalimova.diana@bk.ru>
2025-12-19 21:29:57 +0500

    Update ast.h
    Refactor AST node structure to separate command, redirect, and pipe data into distinct structures.

--- inc/ast.h
+++ inc/ast.h
@@ -3,9 +3,9 @@
 #include "tokens.h"
 
 typedef enum {
-    NODE_COMMAND,
-    NODE_PIPE,
-    NODE_REDIRECT, 
+    NODE_COMMAND,//
+    NODE_PIPE,//
+    NODE_REDIRECT, //
     NODE_AND,
     NODE_OR,
     NODE_SEMICOLON,
@@ -13,24 +13,39 @@ typedef enum {
     NODE_SUBSHELL
 } node_type_t;
 
+
+typedef struct {// изм разделили данные на отдельные структуры
+    char **argv;
+    int argc;
+} command_data_t;// Только для команд
+
+typedef struct {
+    char *in_file;
+    char *out_file;
+    char *err_file;
+    int append;
+} redirect_data_t;// Только для перенаправлений
+
+typedef struct {
+    int redirect_err;// Только для пайпов
+} pipe_data_t;
+
+
 typedef struct ast_node_t {
     node_type_t type;
     struct ast_node_t *left;
     struct ast_node_t *right;
     
-    char **argv; 
-    int argc;
-    
-    char *in_file;
-    char *out_file;
-    char *err_file;
-    int append;
-    int redirect_err;
+    union {
+        command_data_t command;// Исп только когда type == NODE_COMMAND
+        pipe_data_t pipe;//когда type == NODE_PIPE  
+        redirect_data_t redirect;//когда type == NODE_REDIRECT
+    } data;//для остальных типов не нужно дополнительных данных
 } ast_node_t;
 
 ast_node_t *ast_create_node(node_type_t type);
 void ast_destroy(ast_node_t *node);
-ast_node_t *ast_create_command_node(char **argv, int argc); 
+ast_node_t *ast_create_command_node(char **argv, int argc);
 void ast_add_redirect(ast_node_t *node, char *file, int type, int is_append);
 void ast_print(ast_node_t *node, int depth);
 
