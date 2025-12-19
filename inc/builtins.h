54a84c52f15ccbc030279fcf9e1cd405fdf641b7
Diana Mursalimova <mursalimova.diana@bk.ru>
2025-12-19 21:27:41 +0500

    Update builtins.h

--- inc/builtins.h
+++ inc/builtins.h
@@ -1,17 +1,18 @@
 #ifndef BUILTINS_H
 #define BUILTINS_H
 
+// Встроенные команды shell
 int builtin_cd(char **argv);
 int builtin_pwd(char **argv);
 int builtin_echo(char **argv);
 int builtin_exit(char **argv);
 int builtin_help(char **argv);
-
-int builtin_jobs(char **argv);
+int builtin_jobs(char **argv);//добавила
 int builtin_fg(char **argv);
 int builtin_bg(char **argv);
 int builtin_kill(char **argv);
 
+// Функции для работы с встроенными командами
 int is_builtin_command(char *command);
 int handle_builtin(char **argv);
 
